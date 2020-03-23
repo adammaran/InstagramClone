5ec74e6a219b81800bb6ac8d347685c586ec3403
Adam Maran <adam.maran@hotmail.com>
2020-03-23 22:39:19 +0100

    adding users and auth

---
+++ app/src/main/java/com/example/instagramclone/Adapters/ViewHolders/PorfilePostAdapter.java
@@ -0,0 +1,33 @@
+package com.example.instagramclone.Adapters.ViewHolders;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PorfilePostAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ProfilePostViewHolder extends RecyclerView.ViewHolder{
+
+        public ProfilePostViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
