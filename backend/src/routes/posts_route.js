79383b37c2a90b74f27cdd9cd5f3ce2a00881173
Dejan <ljutkee@gmail.com>
2020-05-29 17:02:41 +0200

    route param syntax fix

--- backend/src/routes/posts_route.js
+++ backend/src/routes/posts_route.js
@@ -7,7 +7,7 @@ const auth = require("../middlewares/authMiddleware");
 router.get('/feed/:page', auth, posts.getFeed);
 router.get('/explore/:page', auth, posts.getExplore);
 router.post('/create', auth, image.upload.single('image'), posts.create);
-router.patch('/edit/:id', auth, posts.edit);
+router.patch('/edit/:post_id', auth, posts.edit);
 router.patch('/like/:post_id', auth, posts.like);
 router.patch('/unlike/:post_id', auth, posts.unlike);
 router.patch('/comment/:post_id', auth, posts.comment);
